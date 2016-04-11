0ed34c16ea5a84a3862d7db2b1268d6504ef9fa0
JClerc <jices25@gmail.com>
2016-04-11 14:47:21 +0200

    Improved Facebook process

--- htdocs/app/controller/index.class.php
+++ htdocs/app/controller/index.class.php
@@ -12,33 +12,33 @@ class Index extends Controller {
     public function index(Request $request) {
         // No view are explicitely defined, so it will use View\Index::index()
 
-        if (isset($_SESSION['fb_access_token'])) {
+        $this->set('username', 'INCONNU');
+
+        if ($this->session->has('fb_access_token')) {
 
             $fb = $this->facebook;
 
             try {
-              // Returns a `Facebook\FacebookResponse` object
-              $response = $fb->get('/me?fields=id,name', $_SESSION['fb_access_token']);
+                // Returns a `Facebook\FacebookResponse` object
+                $response = $fb->get('/me?fields=id,name', $this->session->get('fb_access_token'));
+                
+                $user = $response->getGraphUser();
+                $this->set('username', $user->getName());
+
             } catch(\Facebook\Exceptions\FacebookResponseException $e) {
-              // echo 'Graph returned an error: ' . $e->getMessage();
-              // exit;
+                // echo 'Graph returned an error: ' . $e->getMessage();
+                // exit;
             } catch(\Facebook\Exceptions\FacebookSDKException $e) {
-              // echo 'Facebook SDK returned an error: ' . $e->getMessage();
-              // exit;
+                // echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                // exit;
             }
 
-            $user = $response->getGraphUser();
-
-            $this->set('username', $user->getName());
-
-        } else {
-            $this->set('username', 'INCONNU');
         }
 
         $helper = $fb->getRedirectLoginHelper();
 
         $permissions = ['email']; // Optional permissions
-        $this->set('loginUrl', e($helper->getLoginUrl('http://localhost:8888/facebook/connect/', $permissions)));
+        $this->set('loginUrl', $helper->getLoginUrl('http://localhost:8888/facebook/connect/', $permissions));
 
     }
 
