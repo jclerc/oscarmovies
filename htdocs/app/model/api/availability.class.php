bf05f74b0baa5dc0e6e5558923efae68fd9fe2d0
JClerc <jices25@gmail.com>
2016-04-12 16:35:09 +0200

    Encode url parameters

--- htdocs/app/model/api/availability.class.php
+++ htdocs/app/model/api/availability.class.php
@@ -12,7 +12,7 @@ class Availability extends Api {
     const API_RENTAL_AVAILABILITY = 'http://www.canistream.it/services/query?movieId={id}&attributes=1&mediaType=rental';
 
     public function get($filmName) {
-        $json = $this->callJson('find.film', ['name' => $filmName]);
+        $json = $this->callJson('find.film', ['name' => urlencode($filmName)]);
         if (!empty($json) and count($json) > 0 and !empty($json[0]) and !empty($json[0]->_id)) {
             $id = $json[0]->_id;
             $rental = $this->callJson('rental.availability', ['id' => $id]);
