c1dde421a8756c8f1a4225a619bf62835b1e7e08
JClerc <jices25@gmail.com>
2016-04-12 22:27:10 +0200

    Use default message in constructor

--- htdocs/app/exception/user.class.php
+++ htdocs/app/exception/user.class.php
@@ -9,12 +9,12 @@ class UserException extends Exception {
 
     public function __construct($message = null, $code = 0, Exception $previous = null) {
 
+        // Add a default message
+        if (!isset($message)) $message = 'Une erreur est survenue';
+        
         // Call parent constructor
         parent::__construct($message, $code, $previous);
 
-        // Add a default message
-        if (empty($message)) $this->message = 'Une erreur est survenue';
-        
     }
 
 }
