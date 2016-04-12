ccdc5b708f80ae2bc1685a62f19d481e362aa9cc
JClerc <jices25@gmail.com>
2016-04-12 18:25:45 +0200

    Add navbar

--- htdocs/app/template/parts/navbar/default.php
+++ htdocs/app/template/parts/navbar/default.php
@@ -3,35 +3,35 @@
 
     <div class="top-nav">
         <div class="group">
-            <div class="entry entry-large">
-                <img src="/assets/img/ghost/main.svg" alt="" class="entry-icon">
-            </div>
+            <a href="#" class="entry entry-large">
+                <img src="/assets/img/ghost/main.svg" alt="" class="img-responsive">
+            </a>
         </div>
 
         <div class="group">
-            <div class="entry">
-                <img src="/assets/img/icons/chat.svg" alt="" class="entry-icon active">
-            </div>
-            <div class="entry">
-                <img src="/assets/img/icons/trending.svg" alt="" class="entry-icon">
-            </div>
-            <div class="entry">
-                <img src="/assets/img/icons/stats.svg" alt="" class="entry-icon">
-            </div>
-            <div class="entry">
-                <img src="/assets/img/icons/history.svg" alt="" class="entry-icon">
-            </div>
+            <a href="/chat/" class="entry entry-link active">
+                <img src="/assets/img/icons/chat.svg" alt="" class="img-responsive">
+            </a>
+            <a href="/trending/" class="entry entry-link">
+                <img src="/assets/img/icons/trending.svg" alt="" class="img-responsive">
+            </a>
+            <a href="/stats/" class="entry entry-link">
+                <img src="/assets/img/icons/stats.svg" alt="" class="img-responsive">
+            </a>
+            <a href="/history/" class="entry entry-link">
+                <img src="/assets/img/icons/history.svg" alt="" class="img-responsive">
+            </a>
         </div>
     </div>
 
     <div class="bottom-nav">
         <div class="group">
-            <div class="entry entry-large">
-                <img src="/assets/img/user/default-avatar.jpg" alt="" class="entry-icon">
-            </div>
-            <div class="entry">
-                <img src="/assets/img/icons/logout.svg" alt="" class="entry-icon">
-            </div>
+            <a href="#" class="entry entry-large">
+                <img src="/assets/img/user/default-avatar.jpg" alt="" class="img-responsive img-round">
+            </a>
+            <a href="/logout/" class="entry entry-link">
+                <img src="/assets/img/icons/logout.svg" alt="" class="img-responsive">
+            </a>
         </div>        
     </div>
 
