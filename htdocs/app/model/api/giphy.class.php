2e6a45864fd3e7567de903a6f3477d8ab429c619
JClerc <jices25@gmail.com>
2016-04-13 18:33:04 +0200

    Improved giphy

--- htdocs/app/model/api/giphy.class.php
+++ htdocs/app/model/api/giphy.class.php
@@ -8,17 +8,16 @@ use Model\Base\Api;
  */
 class Giphy extends Api {
 
-    const API_SEARCH = 'http://api.giphy.com/v1/gifs/search?q={query}&limit=3&api_key=dc6zaTOxFJmzC';
+    const API_SEARCH = 'http://api.giphy.com/v1/gifs/search?q={query}&limit=10&api_key=dc6zaTOxFJmzC';
 
     public function get($query) {
         $json = $this->callJson('search', ['query' => urlencode($query)]);
-        var_dump($json); exit;
         if (!empty($json) and !empty($json->data)) {
             $max = count($json->data);
-            $index = rand(0, $max-2);
+            $index = rand(0, $max-1);
             $gif = $json->data[$index];
             if (!empty($gif)) {
-                return $gif->embed_url;
+                return $gif->images->original->url;
             }
         }
         return null;
