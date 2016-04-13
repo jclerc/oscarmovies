cf801bf69e65816cf1bf21f1a0a805e83ccb9e36
vanessachaddouk <vanessa.chaddouk@gmail.com>
2016-04-13 19:46:22 +0200

    History
    image carousel almost done

--- htdocs/app/template/page/history/index.php
+++ htdocs/app/template/page/history/index.php
@@ -1,7 +1,7 @@
 <div class="history">
 	<div class="title">
 		<h2>Oscar's previous recommendations</h2>
-		<h3>Find out what Oscar has recommended to you</h3>
+		<h3>Remember what Oscar has showed you</h3>
 	</div>
 
 
@@ -9,7 +9,7 @@
   <div class="indicator"></div>
   <div class="wrap">
     <ul>
-      <li><img src="https://unsplash.it/200/300/?random"/></li>
+      <li class="active"><img src="https://unsplash.it/200/300/?random"/></li>
       <li><img src="https://unsplash.it/200/300/?random=2"/></li>
       <li><img src="https://unsplash.it/200/300/?random=3"/></li>
       <li><img src="https://unsplash.it/200/300/?random=4"/></li>
