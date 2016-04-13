5fa43553572dd2243e4f58bab7e31d910edae035
vanessachaddouk <vanessa.chaddouk@gmail.com>
2016-04-13 15:06:58 +0200

    History page + beginning
    I began coding the history page with a carousel inside

---
+++ htdocs/app/template/page/history/index.php
@@ -0,0 +1,18 @@
+<div class="history">
+	<div class="title">
+		<h2>Oscar's previous recommendations</h2>
+		<h3>Find out what Oscar has recommended to you</h3>
+	</div>
+	
+	<div class="carousel">
+		<div class="slides">
+			<div class="items">
+				<div class="item" style="background-image:url(http://	loremflickr.com/320/240?random=1)">ITEM 1</div>
+				<div class="item" style="background-image:url(http://	loremflickr.com/320/240?random=2)">ITEM 2</div>
+				<div class="item" style="background-image:url(http://	loremflickr.com/320/240?random=3)">ITEM 3</div>
+				<div class="item" style="background-image:url(http://	loremflickr.com/320/240?random=4)">ITEM 4</div>
+				<div class="item" style="background-image:url(http://	loremflickr.com/320/240?random=5)">ITEM 5</div>
+			</div>
+		</div>
+	</div>
+</div>
