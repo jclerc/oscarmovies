f401d56fcb30e6081bb9cf44a64feabd56f150a2
JClerc <jices25@gmail.com>
2016-04-14 18:52:27 +0200

    Starting with Wit.ai

--- htdocs/app/controller/chat.class.php
+++ htdocs/app/controller/chat.class.php
@@ -12,56 +12,63 @@ class Chat extends Controller {
     public function index(Request $request) {
         // No view are explicitely defined, so it will use View\Chat::index()
 
-        if ($request->isAjax()) {
+        if (!$request->isAjax()) {
+
+            // We are starting a new conversation
+            $this->session->set('converse.id', str_replace('.', '', uniqid('', true)));
+            $this->session->delete('converse.data');
+
+        } else {
 
             $post = $request->getAjax();
             $message = $post['message'];
+            $converseId = $this->session->get('converse.id');
 
             // PROCESS API
             $data = [];
-            $data['message'] = call_user_func(function () {
-                $msg = [
-                    'Please wait..',
-                    'I am Oscar ! 👻',
-                    'What do you want ?',
-                    'Ok dude.',
-                    'You are ugly.',
-                    'I love potatoes !',
-                    'STOP TALKING TO ME.',
-                ];
-                return $msg[array_rand($msg)];
-            });
+            // $data['message'] = call_user_func(function () {
+            //     $msg = [
+            //         'Please wait..',
+            //         'I am Oscar ! 👻',
+            //         'What do you want ?',
+            //         'Ok dude.',
+            //         'You are ugly.',
+            //         'I love potatoes !',
+            //         'STOP TALKING TO ME.',
+            //     ];
+            //     return $msg[array_rand($msg)];
+            // });
 
-            if (stripos($message, 'gif') > -1) {
-                if (stripos($message, 'gif ') === 0) {
-                    $data['gif'] = $this->api->giphy->get(substr($message, 4));
-                } else {
-                    $data['gif'] = $this->api->giphy->get('happy');
-                }
-            } else if (stripos($message, 'cat') > -1) {
-                $data['gif'] = $this->api->giphy->get('cat');
-                $data['message'] = 'I HEARD YOU SAY CAT ?';
-            } else if (stripos($message, 'i want to see a ') === 0) {
-                $genre = substr($message, strlen('i want to see a '));
-                $movies = $this->api->movies->findByGenre($genre);
-                if (isset($movies->results) and count($movies->results) > 0) {
+            // if (stripos($message, 'gif') > -1) {
+            //     if (stripos($message, 'gif ') === 0) {
+            //         $data['gif'] = $this->api->giphy->get(substr($message, 4));
+            //     } else {
+            //         $data['gif'] = $this->api->giphy->get('happy');
+            //     }
+            // } else if (stripos($message, 'cat') > -1) {
+            //     $data['gif'] = $this->api->giphy->get('cat');
+            //     $data['message'] = 'I HEARD YOU SAY CAT ?';
+            // } else if (stripos($message, 'i want to see a ') === 0) {
+            //     $genre = substr($message, strlen('i want to see a '));
+            //     $movies = $this->api->movies->findByGenre($genre);
+            //     if (isset($movies->results) and count($movies->results) > 0) {
 
-                    $movie = $movies->results[ rand(0, count($movies->results) - 1) ];
-                    $data['message'] = 'What about ' . $movie->title . ' ?';
+            //         $movie = $movies->results[ rand(0, count($movies->results) - 1) ];
+            //         $data['message'] = 'What about ' . $movie->title . ' ?';
 
-                    $availability = $this->api->availability->get($movie->title);
-                    if (!empty($availability)) {
-                        $services = [];
-                        foreach ($availability as $key => $value) {
-                            $services[] = ucfirst(substr($key, 0, strpos($key, '_')));
-                        }
-                        $data['message'] .= ' You can watch it on ' . implode(', ', $services) . '.';
-                    }
+            //         $availability = $this->api->availability->get($movie->title);
+            //         if (!empty($availability)) {
+            //             $services = [];
+            //             foreach ($availability as $key => $value) {
+            //                 $services[] = ucfirst(substr($key, 0, strpos($key, '_')));
+            //             }
+            //             $data['message'] .= ' You can watch it on ' . implode(', ', $services) . '.';
+            //         }
 
-                } else {
-                    $data['message'] = 'I DONT KNOW WHAT KIND OF GENRE "' . $genre . '" IS';
-                }
-            }
+            //     } else {
+            //         $data['message'] = 'I DONT KNOW WHAT KIND OF GENRE "' . $genre . '" IS';
+            //     }
+            // }
 
             $this->view($data);
 
---
+++ htdocs/app/model/api/wit.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Model\Api;
+use Model\Base\Api;
+use Model\Mapper\Cache;
+
+/**
+ * Wit API
+ */
+class Wit extends Api {
+
+    // const API_CALl = 'http://ip-api.com/json/{ip}';
+
+    // public function get($ip) {
+    //     return $this->callJson('ip', ['ip' => $ip], Cache::EXPIRE_WEEK);
+    // }
+
+}
